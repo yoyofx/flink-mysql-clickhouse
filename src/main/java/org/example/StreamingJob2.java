ce0be7b2a8ac4b1605754b2a8ab2328127353b82
yoyofx <zl.hxd@hotmail.com>
2023-03-02 11:34:06 +0800

    stream windows and batch them.

--- src/main/java/org/example/StreamingJob2.java
+++ src/main/java/org/example/StreamingJob2.java
@@ -1,16 +1,28 @@
 package org.example;
 
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.jsonschema.JsonSerializableSchema;
+import com.google.common.collect.Lists;
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.connector.kafka.source.KafkaSource;
 import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
+import org.apache.flink.formats.json.JsonRowDataDeserializationSchema;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.windowing.ProcessWindowFunction;
 import org.apache.flink.streaming.api.windowing.assigners.TumblingProcessingTimeWindows;
 import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
 import org.apache.flink.util.Collector;
+import org.example.fx.CancalBinlogRow;
+
+import java.sql.Timestamp;
+import java.util.List;
 
 public class StreamingJob2 {
 
@@ -20,27 +32,48 @@ public class StreamingJob2 {
 
         System.out.println("Window WordCount");
 
+//        String str = "{\"data\":[{\"id\":\"1\",\"job_id\":\"1\",\"glue_type\":\"1\",\"glue_source\":\"1\",\"glue_remark\":\"1\",\"add_time\":\"2023-03-01 13:45:15\",\"update_time\":\"2023-03-01 13:45:21\"}],\"database\":\"xxl-job\",\"es\":1677649523000,\"gtid\":\"\",\"id\":3,\"isDdl\":false,\"mysqlType\":{\"id\":\"int(11)\",\"job_id\":\"int(11)\",\"glue_type\":\"varchar(50)\",\"glue_source\":\"mediumtext\",\"glue_remark\":\"varchar(128)\",\"add_time\":\"datetime\",\"update_time\":\"datetime\"},\"old\":null,\"pkNames\":[\"id\"],\"sql\":\"\",\"sqlType\":{\"id\":4,\"job_id\":4,\"glue_type\":12,\"glue_source\":2005,\"glue_remark\":12,\"add_time\":93,\"update_time\":93},\"table\":\"xxl_job_logglue\",\"ts\":1677649523714,\"type\":\"INSERT\"}";
+//
+//        ObjectMapper jsonMapper = new ObjectMapper();
+//        CancalBinlogRow rows = jsonMapper.readValue(str,CancalBinlogRow.class);
+//        System.out.println(rows);
+
         KafkaSource<String> source = KafkaSource.<String>builder()
                 .setBootstrapServers("10.168.4.184:9092")
                 .setTopics("test")
                 .setGroupId("my-group")
-                .setStartingOffsets(OffsetsInitializer.earliest())
+                .setStartingOffsets(OffsetsInitializer.timestamp(Timestamp.valueOf("2023-03-01 00:00:00").getTime()))
                 .setValueOnlyDeserializer(new SimpleStringSchema())
                 .build();
 
 
-        DataStream<Tuple2<String, Integer>> dataStream = env
-                .fromSource(source, WatermarkStrategy.noWatermarks(),"kafka source")
-                //.socketTextStream("localhost", 9999)
-                .flatMap(new StreamingJob2.Splitter())
-                .keyBy(value -> value.f0)
-                .window(TumblingProcessingTimeWindows.of(Time.seconds(5)))
-                .sum(1);
+//        DataStream<Tuple2<String, Integer>> dataStream = env
+//                .fromSource(source, WatermarkStrategy.noWatermarks(),"kafka source")
+//                //.socketTextStream("localhost", 9999)
+//                .flatMap(new StreamingJob2.Splitter())
+//                .keyBy(value -> value.f0)
+//                .window(TumblingProcessingTimeWindows.of(Time.seconds(5)))
+//                .sum(1);
 
 //		DataStream<String> dataStream = env
 //				.socketTextStream("localhost", 9999);
+        ObjectMapper jsonMapper = new ObjectMapper();
 
-        dataStream.print().setParallelism(1);
+        env.fromSource(source, WatermarkStrategy.noWatermarks(),"kafka source")
+                .map(jsonLine -> jsonMapper.readValue(jsonLine, CancalBinlogRow.class))
+                .keyBy(value -> value.getDatabase())
+                .window(TumblingProcessingTimeWindows.of(Time.seconds(15)))
+                .process(new ProcessWindowFunction<CancalBinlogRow, List<CancalBinlogRow>,String, TimeWindow>(){
+                    @Override
+                    public void process(String key, ProcessWindowFunction<CancalBinlogRow, List<CancalBinlogRow> , String, TimeWindow>.Context context, Iterable<CancalBinlogRow> input, Collector<List<CancalBinlogRow>> out) throws Exception {
+                        List<CancalBinlogRow> list = Lists.newArrayList(input);
+                        if (list.size() > 0) {
+                            out.collect(list);
+                        }
+                    }
+                })
+                .print();
+
 
         env.execute("Window WordCount");
     }
---
+++ src/main/java/org/example/fx/CancalFormatCkSink.java
@@ -0,0 +1,83 @@
+package org.example.fx;
+
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.streaming.api.functions.sink.RichSinkFunction;
+import ru.yandex.clickhouse.ClickHouseConnection;
+import ru.yandex.clickhouse.ClickHouseDataSource;
+import ru.yandex.clickhouse.ClickHouseStatement;
+import ru.yandex.clickhouse.settings.ClickHouseProperties;
+import ru.yandex.clickhouse.settings.ClickHouseQueryParam;
+
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class CancalFormatCkSink extends RichSinkFunction<List<CancalBinlogRow>> {
+    private ClickHouseConnection conn = null;
+    private String ckHost = "";
+    private String ckPort = "";
+    private String user = "";
+    private String password = "";
+    private String dbName = "";
+
+    public CancalFormatCkSink withHost(String ckHost,String ckPort) {
+        this.ckHost = ckHost;
+        this.ckPort = ckPort;
+        return this;
+    }
+
+    public CancalFormatCkSink withAuth(String db,String userName,String passWord) {
+        this.dbName = db;
+        this.user = userName;
+        this.password = passWord;
+        return this;
+    }
+
+
+    @Override
+    public void open(Configuration parameters) throws Exception {
+        super.open(parameters);
+
+        String url = String.format("jdbc:clickhouse://%s:%s/%s",this.ckHost,this.ckPort,this.dbName);
+        ClickHouseProperties properties = new ClickHouseProperties();
+        properties.setUser(this.user);
+        properties.setPassword(this.password);
+        properties.setSessionId("default-session-id");
+
+        ClickHouseDataSource dataSource = new ClickHouseDataSource(url, properties);
+        conn = dataSource.getConnection();
+        conn.setAutoCommit(false);
+    }
+
+    @Override
+    public void close() throws Exception {
+        super.close();
+        if (conn != null)
+        {
+            conn.close();
+        }
+    }
+
+    @Override
+    public void invoke(List<CancalBinlogRow> rows, Context context) throws Exception {
+        PreparedStatement ps = null;
+        try {
+            for(CancalBinlogRow row : rows) {
+                ps = conn.prepareStatement("");
+//                ps.setObject();
+            }
+            ps.executeBatch();
+            conn.commit();
+            ps.close();
+        }
+        catch (SQLException e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
+
